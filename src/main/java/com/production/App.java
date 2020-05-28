4fe8bb70a472ccde174e2ddf4a442f640b0470c6
leogtzr <leogutierrezramirez@gmail.com>
2020-05-28 18:57:38 -0500

    remove unnecessary imports

--- src/main/java/com/production/App.java
+++ src/main/java/com/production/App.java
@@ -1,15 +1,10 @@
 package com.production;
 
 import com.production.domain.WorkOrderInformation;
-import com.production.util.Constants;
 import com.production.util.ProductionUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class App {
@@ -23,7 +18,7 @@ public class App {
 
         // TODO: Use a constant for this ...
         ProductionUtils.reconcileInformationFromAgeFile("Age  by WC.xls", workOrderInformationItems);
-         workOrderInformationItems.forEach(System.out::println);
+        workOrderInformationItems.forEach(System.out::println);
     }
 
 }
